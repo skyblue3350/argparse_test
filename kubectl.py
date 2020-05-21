4b6a3e61d80ee00796ab53db22f758cd59824542
sky <skyblue3350@gmail.com>
2020-05-22 03:04:30 +0900

    first commit

---
+++ kubectl.py
@@ -0,0 +1,29 @@
+import argparse
+
+
+def command_get(args):
+    print(args)
+
+
+def command_delete(args):
+    print(args)
+
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+parser_get = subparsers.add_parser("get", help="get resource")
+parser_delete = subparsers.add_parser("delete", help="delete resource")
+
+parser_get.add_argument("resource")
+parser_get.add_argument("resource_name")
+parser_get.set_defaults(handler=command_get)
+
+parser_delete.add_argument("resource")
+parser_delete.add_argument("resource_name")
+parser_delete.set_defaults(handler=command_delete)
+
+args = parser.parse_args()
+
+args.handler(args)
+
